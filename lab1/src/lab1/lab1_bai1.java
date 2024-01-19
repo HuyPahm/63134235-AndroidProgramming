cfa683c3ad990deefc6084dc650bc29ba5b70c59
HuyPahm <148019963+HuyPahm@users.noreply.github.com>
2024-01-19 20:11:44 +0700

    lab1bai1
    lab1bai1

---
+++ lab1/src/lab1/lab1_bai1.java
@@ -0,0 +1,16 @@
+package lab1;
+
+
+
+public class lab1_bai1 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Họ và tên: ");
+		String hoTen = scanner.nextLine();
+		System.out.print("Điểm TB: ");
+		double diemTB = scanner.nextDouble();
+		System.out.printf("%s %f điểm", hoTen, diemTB); 
+		scanner.close(); 
+	}
+}
