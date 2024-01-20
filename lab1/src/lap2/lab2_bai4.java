5e40b263699183a1270064177e804cb98c28cd02
HuyPahm <148019963+HuyPahm@users.noreply.github.com>
2024-01-20 11:43:21 +0700

    Create lab2_bai4.java

---
+++ lab1/src/lap2/lab2_bai4.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package lap2;
+
+/**
+ * 
+ */
+import java.util.Scanner;
+public class lab2_bai4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner (System.in);
+		
+		System.out.println("----- MENU -----");
+        System.out.println("1. Giải phương trình bậc nhất");
+        System.out.println("2. Giải phương trình bậc 2");
+        System.out.println("3. Tiền điện");
+        System.out.println("4. Thoát");
+
+	}
+
+}
