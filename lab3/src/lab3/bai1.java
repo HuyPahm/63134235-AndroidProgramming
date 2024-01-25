3c0cdfee10ebfc93c1acc6e2cc576d0de5ad0fa2
HuyPahm <148019963+HuyPahm@users.noreply.github.com>
2024-01-25 11:33:29 +0700

    Update bai1.java

--- lab3/src/lab3/bai1.java
+++ lab3/src/lab3/bai1.java
@@ -6,6 +6,7 @@ package lab3;
 /**
  * 
  */
+import java.util.Scanner;
 public class bai1 {
 
 	/**
@@ -13,7 +14,25 @@ public class bai1 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("nhap n: ");
+		double n = scanner.nextDouble();
+		boolean ok = true;
+	for(int i=2;i<=n-1;i++)
+	{
+		if(n%i==0)
+		{
+			ok =false;
+			break;
+		}
+		
+	}
+	if(ok) {
+		System.out.println(n+ " là số nguyên tố");
+	}else {
+		System.out.println(n+  " k phải");
+	}
+	scanner.close();
 	}
 
 }
