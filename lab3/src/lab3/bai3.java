6d3697dbd4c14735c61e72df4e0b4d086322c9a8
HuyPahm <148019963+HuyPahm@users.noreply.github.com>
2024-01-26 23:36:46 +0700

    xong lab3_3

--- lab3/src/lab3/bai3.java
+++ lab3/src/lab3/bai3.java
@@ -31,17 +31,42 @@ public class bai3 {
         	System.out.print("phan tu thu" + (i + 1) + ": ");
             arr[i] = scanner.nextInt();
         }
+        System.out.println("mang da nhap: ");
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
-        
+       
+        //1
         Arrays.sort(arr);
 
         System.out.println("\nmang da sap xep:");
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
-
+        //2
+        int min = arr[0];
+//        for(int i=1;i<n;i++)
+//        {
+//        	min = Math.min(min,arr[i]);
+//        }
+        for(int i=1;i<n;i++) {
+        	if(arr[i]< min) {
+        		min = arr[i];
+        	}
+        }
+        System.out.println("\nmin : "+ min);
+        //3
+        int s =0;
+        int dem=0;
+        for(int i=0;i<n;i++) {
+        	if(arr[i] %3 ==0) {
+        		s+=arr[i];
+        		dem++;
+       
+        	}
+        } 
+        double kq = s/dem;
+        System.out.println("TBC: "+kq);
         scanner.close();
 		
 	}
