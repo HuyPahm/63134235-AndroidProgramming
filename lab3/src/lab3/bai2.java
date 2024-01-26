fc973428787ecdda2d9fe645aaccc56d617241fb
HuyPahm <148019963+HuyPahm@users.noreply.github.com>
2024-01-26 18:17:14 +0700

    complete lab3_2

--- lab3/src/lab3/bai2.java
+++ lab3/src/lab3/bai2.java
@@ -16,12 +16,18 @@ public class bai2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("nhap n: ");
-		int x= scanner.nextInt();
-		for(int i=1;i<=10;i++)
-		{
-			System.out.printf("%d x%d = %d \n",x,i,x*i );
+//		System.out.print("nhap n: ");
+//		int x= scanner.nextInt();
+		for(int j=1;j<=9;j++) {
+			
+			for(int i=1;i<=10;i++)
+			{
+				System.out.printf("%d x %d = %d \n",j,i,j*i );
+			
+			}
 		}
+		
+		
 		
 		scanner.close();
 	}
