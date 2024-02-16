c05bcf49c3bdf0f582ea1eb0d2f005304e353f4b
HuyPahm <148019963+HuyPahm@users.noreply.github.com>
2024-02-16 22:59:36 +0700

    khai code dau xuan

---
+++ lab4/src/lab4/bai1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package lab4;
+
+/**
+ * 
+ */
+
+public class bai1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
