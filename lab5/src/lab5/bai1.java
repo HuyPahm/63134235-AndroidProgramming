bbb52e05bc0433010d6614117737dfc3291072f1
HuyPahm <148019963+HuyPahm@users.noreply.github.com>
2024-02-26 13:33:03 +0700

    Update bai1.java

--- lab5/src/lab5/bai1.java
+++ lab5/src/lab5/bai1.java
@@ -6,6 +6,9 @@ package lab5;
 /**
  * 
  */
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 public class bai1 {
 
 	/**
@@ -14,6 +17,36 @@ public class bai1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		 Scanner scanner = new Scanner(System.in);
 
-}
+	        List<Double> numberList = new ArrayList<>();
+
+	        while (true) {
+	            System.out.print("Nhập: ");
+	            String input = scanner.nextLine();
+
+	            if (input.equalsIgnoreCase("N")) {
+	                break;
+	            }
+
+	            try {
+	                double number = Double.parseDouble(input);
+	                numberList.add(number);
+	            } catch (NumberFormatException e) {
+	                System.out.println("Số sai.");
+	            }
+	        }
+
+	        System.out.println("Danh sách số thực đã nhập:");
+	        for (double number : numberList) {
+	            System.out.println(number);
+	        }
+
+	        double sum = 0;
+	        for (double number : numberList) {
+	            sum += number;
+	        }
+
+	        System.out.println("Tổng của danh sách số thực: " + sum);
+	    }
+	}
